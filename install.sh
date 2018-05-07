e00b74edb154a9b8f7bea808bc212a936d83e34d
zfl9 <j1002238565@gmail.com>
2018-05-07 22:23:47 +0800

    update on 2018-05-07 22:23:47 install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# only for linux x86_64
+
+echo -n "this script is only for x86_64. continue? [y/n] "
+read input
+if [ "$input" = 'n' -o "$input" = 'N' ]; then
+    exit 0
+fi
 
 cp -af tun2socks/tun2socks.x64 /usr/local/bin/tun2socks
 cp -af chinadns/chinadns /usr/local/bin/
 cp -af dnsforwarder/dnsforwarder /usr/local/bin/
 cp -af ss-tun2socks /usr/local/bin/
-
 chmod 0755 /usr/local/bin/tun2socks
 chmod 0755 /usr/local/bin/chinadns
 chmod 0755 /usr/local/bin/dnsforwarder
@@ -18,9 +22,12 @@ cp -af chinadns/chnroute.txt /etc/tun2socks/
 cp -af dnsforwarder/dnsforwarder.conf /etc/tun2socks/
 
 cp -af ss-tun2socks.service /etc/systemd/system/
-echo -n "start ss-tun2socks on boot with systemd ? "
+systemctl daemon-reload
+
+echo -n "start ss-tun2socks on boot with systemd? [y/n] "
 read input
 if [ "$input" = "y" -o "$input" = "Y" ]; then
-    systemctl daemon-reload
     systemctl enable ss-tun2socks.service
 fi
+
+echo "Done!"
