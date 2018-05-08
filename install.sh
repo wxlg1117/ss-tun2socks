f5a18c1c4486ad4b1627b0725f0f6075a169da4d
zfl9 <j1002238565@gmail.com>
2018-05-08 09:16:46 +0800

    update on 2018-05-08 09:16:46 install.sh

--- install.sh
+++ install.sh
@@ -30,4 +30,13 @@ if [ "$input" = "y" -o "$input" = "Y" ]; then
     systemctl enable ss-tun2socks.service
 fi
 
-echo "Done!"
+echo -n "edit /etc/tun2socks/ss-tun2socks.conf? [y/n] "
+read input
+if [ "$input" = "y" -o "$input" = "Y" ]; then
+    vim /etc/tun2socks/ss-tun2socks.conf
+    echo -n "start ss-tun2socks service? [y/n] "
+    read input
+    if [ "$input" = "y" -o "$input" = "Y" ]; then
+        /usr/local/bin/ss-tun2socks start
+    fi
+fi
